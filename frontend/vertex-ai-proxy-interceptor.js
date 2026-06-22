77cc1884c357094c6bcc2f38be5bb17d7b542b83
Kunalbhl <Kunalpareek73@gmail.com>
2026-06-23 01:19:20 +0530

    23/06/2026 01:18am

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': '-QsJ1ui7PPvq0Ba61dXLqxGnA6l4dJiq',
+            'X-App-Proxy': '2i1GfmQruTDRqiflOMRTFFJS6ICZCM8Q',
           },
           body: JSON.stringify(requestDetails),
         };
